8360925e637dd5d0edf7c9825f5408ad10f78cfd
Elisa <kugaevskaya.elizaveta@gmail.com>
2019-02-21 01:28:15 +0900

    Changed directory to relative #22

--- code_build/country_year_rgdpe.R
+++ code_build/country_year_rgdpe.R
@@ -5,10 +5,8 @@ rm(list = ls())
 pacman::p_load(tidyverse)
 
 ## import data
-install.packages(readxl)
-library(readxl) # see http://www.sthda.com/english/wiki/reading-data-from-excel-files-xls-xlsx-into-r
-setwd("/Users/elisa/Desktop/check/unrecognized_states/input")
-pwt <- read_excel("pwt90.xlsx", sheet = 3)
+pacman::p_load(readxl) # see http://www.sthda.com/english/wiki/reading-data-from-excel-files-xls-xlsx-into-r
+pwt <- read_excel("input/pwt90.xlsx", sheet = 3)
 
 ## transform data
 tidy_pwt <- pwt %>% 
@@ -16,6 +14,6 @@ tidy_pwt <- pwt %>%
 tidy_pwt
 
 ## create csv
-write_csv(tidy_pwt, "/Users/elisa/Desktop/check/unrecognized_states/b_temp/country_year_rgdpe.csv")
+write_csv(tidy_pwt, "b_temp/country_year_rgdpe.csv")
 # see https://www.rdocumentation.org/packages/readr/versions/0.1.1/topics/write_csv
 
